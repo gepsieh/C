6da0d5299b1d1efc040d56ced54a6d972bd882c8
Tanaka Hao <314308943@qq.com>
2022-11-12 21:38:10 +0800

    change encoding

--- C Primer Plus/Charpter 3/3.5.c
+++ C Primer Plus/Charpter 3/3.5.c
@@ -2,9 +2,9 @@
  * @Author: Tanaka Hao 314308943@qq.com
  * @Date: 2022-11-12 20:32:01
  * @LastEditors: Tanaka Hao 314308943@qq.com
- * @LastEditTime: 2022-11-12 21:31:22
+ * @LastEditTime: 2022-11-12 21:37:21
  * @FilePath: \C\C Primer Plus\Charpter 3\3.5.c
- * @Description: 输入年龄并显示年龄对应的秒数。
+ * @Description: 输入年龄并转换成秒数
  * 
  * Copyright (c) 2022 by Tanaka Hao 314308943@qq.com, All Rights Reserved. 
  */
@@ -19,7 +19,7 @@ int main(void)
     scanf("%d", &age);
     sec=(long)age*3.156e7;
 
-    printf("你的年龄对应秒数是 %ld.", sec);
+    printf("对应秒数为 %ld.", sec);
 
     getchar();
     getchar();
